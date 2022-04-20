abe2acd9da529549fd82f7559de8be2c72ab94f1
서태욱 <seotaewook@seotaeug-ui-MacBookPro.local>
2022-04-20 09:46:34 +0900

    feat: Static Initializer Block

---
+++ src/StaticInitializerBlock.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+ static int B; // static으로 B,H를 입력 받는다.
+ static int H;
+ static boolean flag = true; //flag 변수를 true로 선언
+ 
+ static {   
+     Scanner sc = new Scanner(System.in); 
+     B = sc.nextInt();
+     H = sc.nextInt();
+     
+     if((B<=0 || H<=0)) {
+         flag = false; // B나 H가 0보다 작으면 false를 내주고 아래 문구를 프린트한다. 
+         System.out.println("java.lang.Exception: Breadth and height must be positive");
+     }
+ }
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class

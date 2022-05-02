729383ba9695f58962bf18b2064a3d2c9e7b85ba
Woogy <apolontes@hanmail.net>
2022-05-02 18:00:10 +0900

    for문으로 다시 풀어봄

--- src/StringReverse.java
+++ src/StringReverse.java
@@ -8,13 +8,27 @@ public class StringReverse {
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
         /* Enter your code here. Print output to STDOUT. */
-        StringBuffer sb = new StringBuffer(A);
 
-        if  (A.equals(sb.reverse().toString())) {
+        String reverse ="";
+        for (int i = A.length() -1; i>=0; i--){
+            reverse = reverse + A.charAt(i);
+        }
+
+        System.out.println(reverse);
+
+        if (A.equals(reverse)){
             System.out.println("Yes");
         } else {
             System.out.println("No");
         }
+
+//        StringBuffer sb = new StringBuffer(A);
+//
+//        if  (A.equals(sb.reverse().toString())) {
+//            System.out.println("Yes");
+//        } else {
+//            System.out.println("No");
+//        }
 
     }
 }

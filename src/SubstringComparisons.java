b5506be901687eb8dc6b3331f93a63f04efda6cb
TaeWook <tw.seo@aifrica.co.kr>
2022-05-03 16:12:12 +0900

    회사 맥북 변경사항

--- src/SubstringComparisons.java
+++ src/SubstringComparisons.java
@@ -6,15 +6,12 @@ public class SubstringComparisons {
         String smallest = "";
         String largest = "";
 
-        // Complete the function
-        // 'smallest' must be the lexicographically smallest substring of length 'k'
-        // 'largest' must be the lexicographically largest substring of length 'k'
-        String lex = s.substring(0, k);
-        smallest = largest = lex;  // wel
-//                                5       2
-        for (int i=1; i<= (s.length() - k); i++){
-            lex = s.substring(i,(i+k)); // abcde  (1,3) bc (2,4) cd (3,5) de  // s.substring(i, i+k) (1,3) bc (2,4)cd (3,5) de (4,6) e?
-            if (lex.compareTo(smallest)<0){  //   (1,4) bcd (2,5) cde // (1,4) bcd (2,5) cde (3,6) de?
+        String lex = s.substring(0,k);
+        smallest = largest = lex;
+
+        for (int i=1; i<=s.length()-k; i++){
+            lex = s.substring(i,i+k);
+            if (lex.compareTo(smallest)<0){
                 smallest = lex;
             }
             if (lex.compareTo(largest)>0){

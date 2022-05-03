b9c75aa06ca49b5eacabadb3cc4b872fef19b70c
woogys <apolontes@hanmail.net>
2022-05-03 18:25:05 +0900

    Java ArrayList

--- src/JavaArraylist.java
+++ src/JavaArraylist.java
@@ -8,5 +8,27 @@ public class JavaArraylist {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<ArrayList> lines = new ArrayList<ArrayList>();
+        for(int i=0; i<n; i++) {
+            int d = sc.nextInt();
+
+            ArrayList<Integer> line = new ArrayList<Integer>();
+            for(int j=0; j<d; j++) line.add(sc.nextInt());
+            lines.add(line);
+        }
+
+        int q = sc.nextInt();
+        for(int i=0; i<q; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            try {
+                System.out.println( lines.get(x-1).get(y-1) );
+            }
+            catch(IndexOutOfBoundsException e) {
+                System.out.println("ERROR!");
+            }
+        }
     }
 }

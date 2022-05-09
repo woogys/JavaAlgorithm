e091275719e1f6a162877a0c367d619b624ceba9
Woogys <apolontes@hanmail.net>
2022-05-10 00:03:57 +0900

    Set<> 변수명 = new HashSet<>(); 형태로 유연하게 변경

--- src/JavaHashset.java
+++ src/JavaHashset.java
@@ -18,7 +18,7 @@ public class JavaHashset {
         }
 
 //Write your code here
-        HashSet<String> pairs = new HashSet<>();
+        Set<String> pairs = new HashSet<>();
         for(int i=0; i<t; i++){
             pairs.add(pair_left[i]+" "+pair_right[i]);
             System.out.println(pairs.size());
